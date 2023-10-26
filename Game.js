667c38ac6ae16b0e19c82462eb4e0a91fee52399
abcd2131 <aarne.leppakoski29@gmail.com>
2023-10-26 17:08:18 +0300

    small changes

--- Game.js
+++ Game.js
@@ -8,30 +8,62 @@ const startButton = document.getElementById('start_btn');
 const optionsButton = document.getElementById('options_button');
 const questContainer = document.querySelector('.container');
 let backgroundImage = new Image();
+const scene1Container = document.getElementById('scene-1-container'); //
+const scene1Image = document.getElementById('scene_1Image'); //
 document.body.appendChild(canvas);
+
+// Function to handle the transition to Scene 1
+function transitionToScene1() {
+  // Hide the initial content
+  const canvasContainer = document.querySelector('.canvas-container');
+  canvasContainer.style.display = 'none';
+
+  // Show the Scene 1 content
+  scene1Container.style.display = 'block';
+
+  // Load and display the Scene 1 image
+}
+
+// Add an event listener to the "Start" button to transition to Scene 1
+startButton.addEventListener('click', () => {
+  transitionToScene1();
+});
 
 const scenes = {
   menu: () => {
     button1.style.display = 'none';
     button2.style.display = 'none';
-    questContainer.style.display = 'none';
+    //questContainer.style.display = 'none'; // tämä poistaa scene 2 taustakuvan mutta jos se on päällä niin poistaa scene 1 taustakuvan???
     backgroundImage.src = 'images/starter.png';
     backgroundImage.onload = () => {
       ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
     };
   },
   options: () => {},
-  scene_1: () => {},
+  scene_1: () => {
+    startButton.style.display = 'none';
+    questContainer.style.display = 'none';
+    backgroundImage.src = 'images/Scene_1.png';
+    backgroundImage.onload = () => {
+      ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+    };
+    //scenes.menu();
+    // Add an event listener to the "Start" button to switch to the next scene
+    //startButton.addEventListener('click', () => {
+    //scenes.scene_2();
+    //});
+  },
+
   scene_2: () => {},
 };
 
 let currentScene = 'menu';
 
-function piirrä() {
+function piirra() {
   scenes[currentScene]();
 }
 
-piirrä();
+piirra();
 
 // ----------------------TARINAN KULKU---------------------------------------------
 
