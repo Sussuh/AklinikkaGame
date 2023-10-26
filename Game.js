403c74cb30cba1a9e1e178ae6fbfb54bafc45d5e
Massimosch <120001621+Massimosch@users.noreply.github.com>
2023-10-26 15:21:27 +0300

    updt

--- Game.js
+++ Game.js
@@ -6,32 +6,67 @@ const button1 = document.getElementById('btn1');
 const button2 = document.getElementById('btn2');
 const startButton = document.getElementById('start_btn');
 const optionsButton = document.getElementById('options_button');
-const questContainer = document.querySelector('.container');
+const questContainer = document.getElementById('questContainer');
+const kieliYksi = document.getElementById('Kielivaihtoehto1');
+const kieliKaksi = document.getElementById('Kielivaihtoehto2');
 let backgroundImage = new Image();
 document.body.appendChild(canvas);
 
+//--------------------------------------Scenet
 const scenes = {
   menu: () => {
+    backgroundImage.src = 'images/starter.png';
     button1.style.display = 'none';
     button2.style.display = 'none';
-    questContainer.style.display = 'none';
-    backgroundImage.src = 'images/starter.png';
     backgroundImage.onload = () => {
       ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
     };
   },
-  options: () => {},
-  scene_1: () => {},
+  scene_1: () => {
+    backgroundImage.src = 'images/Scene_2.png';
+    startButton.style.display = 'none';
+    kieliYksi.style.display = 'none';
+    kieliKaksi.style.display = 'none';
+    backgroundImage.onload = () => {
+      ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+    };
+  },
   scene_2: () => {},
 };
 
+//---------------------------------------------------------
+
+//-------------------Scenen vaihto Start buttonista------------
+
 let currentScene = 'menu';
+
+startButton.addEventListener('click', function () {
+  const nextSceneName = getNextSceneName(currentScene);
+  if (scenes[nextSceneName]) {
+    currentScene = nextSceneName;
+    piirrä();
+  } else {
+    console.log('Ei ole uusia scenejä.');
+  }
+});
+
+function getNextSceneName(currentSceneName) {
+  switch (currentSceneName) {
+    case 'menu':
+      return 'scene_1';
+    case 'scene_1':
+      return 'scene_2';
+    default:
+      return currentScene;
+  }
+}
 
 function piirrä() {
   scenes[currentScene]();
 }
 
 piirrä();
+//---------------------------------------------------------------------
 
 // ----------------------TARINAN KULKU---------------------------------------------
 
