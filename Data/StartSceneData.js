0ace52ff0265958bb86a0998a2a22ecb9f85c4b3
Massimosch <120001621+Massimosch@users.noreply.github.com>
2023-11-08 14:45:05 +0200

    Timone vois kattoo tätä

--- Data/StartSceneData.js
+++ Data/StartSceneData.js
@@ -1,136 +1,134 @@
 const StartSceneData = {
-  "SofillaOnTietoa_1":
-  {
-    "_comment" : "Tänne voi laittaa random kommentteja scenestä tai muuta?",
-    "type" : "linear",
-    "background" : "koulun_piha",
-    "characters" : null,
-    "text_type" : "infobox",
-    "text_position" : "middle",
-    "text" : "t_sofi_outside_drama",
-    "next_scene" : "SofillaOnTietoa_2"
+  SofillaOnTietoa_1: {
+    _comment: 'Tänne voi laittaa random kommentteja scenestä tai muuta?',
+    type: 'linear',
+    background: 'KoulunPiha',
+    characters: null,
+    text_type: 'infobox',
+    text_position: 'middle',
+    text: 't_sofi_outside_drama',
+    next_scene: 'SofillaOnTietoa_2',
   },
-  "SofillaOnTietoa_2":
-  {
-    "type" : "options",
-    "background" : "koulun_piha",
-    "characters" : ["npc_sofi_anxious"],
-    "text_type" : "dialogue",
-    "text_position" : "dialogue_left",
-    "text" : "sofi_outside_drama",
-    "player_options" : 
-    [{
-      "text" : "t_sofi_outside_option_1",
-      "next_scene" : "SofillaOnTietoaRoskiksille"
-    },
-    {
-      "text" : "t_sofi_outside_option_2",
-      "next_scene" : "SofillaOnTietoaHaloo"
-    }]
+  SofillaOnTietoa_2: {
+    type: 'options',
+    background: 'koulun_piha',
+    characters: ['npc_sofi_anxious'],
+    text_type: 'dialogue',
+    text_position: 'dialogue_left',
+    text: 'sofi_outside_drama',
+    player_options: [
+      {
+        text: 't_sofi_outside_option_1',
+        next_scene: 'SofillaOnTietoaRoskiksille',
+      },
+      {
+        text: 't_sofi_outside_option_2',
+        next_scene: 'SofillaOnTietoaHaloo',
+      },
+    ],
   },
-  "SofillaOnTietoaHaloo":
-  {
-    "type" : "options",
-    "background" : "koulun_piha",
-    "characters" : ["npc_sofi_angry"],
-    "text_type" : "dialogue",
-    "text_position" : "dialogue_left",
-    "text" : "t_sofi_outside_drama_2",
-    "player_options" : 
-    [{
-      "text" : "t_sofi_outside_drama_option_deny",
-      "next_scene" : "EScene"
-    },
-    {
-      "text" : "t_sofi_outside_drama_option_okay",
-      "next_scene" : "LydianHuolet_1"
-    }]
-  },
-
-  "SofillaOnTietoaRoskiksille":
-  {
-    "comment" : "____________Tästä alkaa terkkari keskustelu",
-    "type" : "linear",
-    "background" : "koulun_piha_roskiksilla",
-    "characters" : null,
-    "text_type" : "infobox",
-    "text_position" : "middle",
-    "text" : "t_sofi_outside_towards_trash_narrator",
-    "next_scene" : "SofillaOnTietoaTerkkarille_1"
-  },
-  "SofillaOnTietoaTerkkarille_1" : 
-  {
-    "type" : "options",
-    "background" : "koulun_piha_roskiksilla",
-    "characters" : null,
-    "text_type" : "infobox",
-    "text_position" : "dialogue_left",
-    "text" : "t_sofi_outisde_nurse",
-    "player_options" : 
-    [{
-      "text" : "t_sofi_outside_nurse_option_yes",
-      "next_scene" : "wip"
-    },
-    {
-      "text" : "t_sofi_outside_nurse_option_no",
-      "next_scene" : "wip"
-    }]
+  SofillaOnTietoaHaloo: {
+    type: 'options',
+    background: 'koulun_piha',
+    characters: ['npc_sofi_angry'],
+    text_type: 'dialogue',
+    text_position: 'dialogue_left',
+    text: 't_sofi_outside_drama_2',
+    player_options: [
+      {
+        text: 't_sofi_outside_drama_option_deny',
+        next_scene: 'EScene',
+      },
+      {
+        text: 't_sofi_outside_drama_option_okay',
+        next_scene: 'LydianHuolet_1',
+      },
+    ],
   },
 
-  "LydianHuolet_1":
-  {
-    "comment" : "tän scenen avulla vois pohtia animaatioita/fadein/hahmo focusta selkeyden vuoksi",
-    "type" : "linear",
-    "background" : "koulun_piha_roskiksilla",
-    "characters" : ["npc_sofi_worried", "npc_lydia"],
-    "text_type" : "dialogue",
-    "text_position" : "dialogue_left",
-    "text" : "t_sofia_lydia_vape_1",
-    "next_scene" : "LydianHuolet_2"
+  SofillaOnTietoaRoskiksille: {
+    comment: '____________Tästä alkaa terkkari keskustelu',
+    type: 'linear',
+    background: 'koulun_piha_roskiksilla',
+    characters: null,
+    text_type: 'infobox',
+    text_position: 'middle',
+    text: 't_sofi_outside_towards_trash_narrator',
+    next_scene: 'SofillaOnTietoaTerkkarille_1',
   },
-  "LydianHuolet_2":
-  {
-    "comment" : "tän scenen avulla vois pohtia animaatioita/fadein/hahmo focusta selkeyden vuoksi",
-    "type" : "linear",
-    "background" : "koulun_piha_roskiksilla",
-    "characters" : ["npc_sofi_worried", "npc_lydia_annoyed"],
-    "text_type" : "dialogue",
-    "text_position" : "dialogue_right",
-    "text" : "t_sofia_lydia_vape_2",
-    "next_scene" : "LydianHuolet_3"
+  SofillaOnTietoaTerkkarille_1: {
+    type: 'options',
+    background: 'koulun_piha_roskiksilla',
+    characters: null,
+    text_type: 'infobox',
+    text_position: 'dialogue_left',
+    text: 't_sofi_outisde_nurse',
+    player_options: [
+      {
+        text: 't_sofi_outside_nurse_option_yes',
+        next_scene: 'wip',
+      },
+      {
+        text: 't_sofi_outside_nurse_option_no',
+        next_scene: 'wip',
+      },
+    ],
   },
-  "LydianHuolet_3":
-  {
-    "comment" : "tän scenen avulla vois pohtia animaatioita/fadein/hahmo focusta selkeyden vuoksi",
-    "type" : "linear",
-    "background" : "koulun_piha_roskiksilla",
-    "characters" : ["npc_sofi_worried", "npc_lydia_annoyed"],
-    "text_type" : "dialogue",
-    "text_position" : "dialogue_left",
-    "text" : "t_sofia_lydia_vape_3",
-    "next_scene" : "LydianHuolet_4"
+
+  LydianHuolet_1: {
+    comment:
+      'tän scenen avulla vois pohtia animaatioita/fadein/hahmo focusta selkeyden vuoksi',
+    type: 'linear',
+    background: 'koulun_piha_roskiksilla',
+    characters: ['npc_sofi_worried', 'npc_lydia'],
+    text_type: 'dialogue',
+    text_position: 'dialogue_left',
+    text: 't_sofia_lydia_vape_1',
+    next_scene: 'LydianHuolet_2',
   },
-  "LydianHuolet_4":
-  {
-    "comment" : "tän scenen avulla vois pohtia animaatioita/fadein/hahmo focusta selkeyden vuoksi",
-    "type" : "linear",
-    "background" : "koulun_piha_roskiksilla",
-    "characters" : ["npc_sofi_worried", "npc_lydia_anxious"],
-    "text_type" : "dialogue",
-    "text_position" : "dialogue_left",
-    "text" : "t_sofia_lydia_vape_4",
-    "next_scene" : "LydianHuolet_5"
+  LydianHuolet_2: {
+    comment:
+      'tän scenen avulla vois pohtia animaatioita/fadein/hahmo focusta selkeyden vuoksi',
+    type: 'linear',
+    background: 'koulun_piha_roskiksilla',
+    characters: ['npc_sofi_worried', 'npc_lydia_annoyed'],
+    text_type: 'dialogue',
+    text_position: 'dialogue_right',
+    text: 't_sofia_lydia_vape_2',
+    next_scene: 'LydianHuolet_3',
   },
-  "LydianHuolet_5":
-  {
-    "comment" : "tän scenen avulla vois pohtia animaatioita/fadein/hahmo focusta selkeyden vuoksi",
-    "type" : "linear",
-    "background" : "koulun_piha_roskiksilla",
-    "characters" : ["npc_sofi_worried", "npc_lydia_anxious"],
-    "text_type" : "dialogue",
-    "text_position" : "dialogue_right",
-    "text" : "t_sofia_lydia_vape_5",
-    "next_scene" : "wip"
-  }
-}
-export default StartSceneData
+  LydianHuolet_3: {
+    comment:
+      'tän scenen avulla vois pohtia animaatioita/fadein/hahmo focusta selkeyden vuoksi',
+    type: 'linear',
+    background: 'koulun_piha_roskiksilla',
+    characters: ['npc_sofi_worried', 'npc_lydia_annoyed'],
+    text_type: 'dialogue',
+    text_position: 'dialogue_left',
+    text: 't_sofia_lydia_vape_3',
+    next_scene: 'LydianHuolet_4',
+  },
+  LydianHuolet_4: {
+    comment:
+      'tän scenen avulla vois pohtia animaatioita/fadein/hahmo focusta selkeyden vuoksi',
+    type: 'linear',
+    background: 'koulun_piha_roskiksilla',
+    characters: ['npc_sofi_worried', 'npc_lydia_anxious'],
+    text_type: 'dialogue',
+    text_position: 'dialogue_left',
+    text: 't_sofia_lydia_vape_4',
+    next_scene: 'LydianHuolet_5',
+  },
+  LydianHuolet_5: {
+    comment:
+      'tän scenen avulla vois pohtia animaatioita/fadein/hahmo focusta selkeyden vuoksi',
+    type: 'linear',
+    background: 'koulun_piha_roskiksilla',
+    characters: ['npc_sofi_worried', 'npc_lydia_anxious'],
+    text_type: 'dialogue',
+    text_position: 'dialogue_right',
+    text: 't_sofia_lydia_vape_5',
+    next_scene: 'wip',
+  },
+};
+export default StartSceneData;
--- micoJS.js
+++ micoJS.js
@@ -1,7 +1,10 @@
+'use strict';
 import StartSceneData from '/Data/StartSceneData.js';
 import Suomi from './Data/suomi.js';
-
-const textField = document.querySelector('#teksti');
+const textField = document.querySelector('.top-infobox');
+const speechBubble = document.querySelector('.SpeechBubble');
+const choiceBox = document.querySelector('.ChoiceBox');
+//const menuContainer = document.querySelector('.menu-container')
 
 let currentBackground;
 let language = Suomi;
@@ -24,29 +27,35 @@ function SceneChange() {
     // event listener here?
     //.addEventListener('click', function () {
     //ChangeScene(currentScene.next_scene);} ???
-    // document
-    //   .getElementById('option-button-1')
-    //   .addEventListener('click', function () {
-    //     selectOption(0);
-    //   });
-    // document
-    //   .getElementById('') //Nimeä
-    //   .addEventListener('click', function () {
-    //     selectOption(1);
-    //   });
   }
   if (currentScene.background != currentBackground) {
     currentBackground = currentScene.background;
     // change background here
+    updateBackground(currentBackground); // Checkkaa console.. en oikein ymmärrä miksei piirrä
   }
   if (currentScene.characters != null) {
     for (let i = 0; i < currentScene.characters.length; i++) {
       // draw characters here
+      drawCharacters(null); // ????????????????
     }
   }
   let textId = currentScene.text;
   textField.innerHTML = language[textId];
 }
-
+//TODO:
 // Vaihtoehdot teksti päivittymään, klikatessa vaihtuu, Consoleen tieto
 // mikä scene....
+
+function updateBackground(backgroundImageSrc) {
+  const backgroundImageElement = document.getElementById('backgroundImage');
+  if (backgroundImageElement) {
+    backgroundImageElement.src = `/image/backgrounds/${backgroundImageSrc}`;
+  }
+}
+
+function drawCharacters(characterImageSrc) {
+  const characterImageElement = document.querySelector('.character');
+  if (characterImageElement) {
+    characterImageElement.src = `/image/characters/${characterImageSrc}`;
+  }
+}
